746ea77f28cf39e86905b334036f6b2d54ef56dd
neharajendran123-cell <neharajendran123@gmail.com>
2026-01-18 22:09:16 +0530

    Create ArrayList.java

---
+++ ArrayList.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class ArrayListExample{
+    public static void main(String[] args){
+        ArrayList<String> a=new ArrayList<String>();
+        a.add("Apple");
+        a.add("Orange");
+        a.add("Strawberry");
+        a.add("Grapes");
+
+        System.out.println(a);
+
+    }
+}
