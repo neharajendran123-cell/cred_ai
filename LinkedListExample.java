fee1dc127b252c71e60f7f252eb458b5f179ff01
neharajendran123-cell <neharajendran123@gmail.com>
2026-01-19 19:42:24 +0530

    Create LinkedListExample.java

---
+++ LinkedListExample.java
@@ -0,0 +1,15 @@
+import java.util.LinkedList;
+
+public class Geeks {
+
+    public static void main(String[] args){
+        LinkedList<String> l = new LinkedList<String>();
+        l.add("One");
+        l.add("Two");
+        l.add("Three");
+        l.add("Four");
+        l.add("Five");
+
+        System.out.println(l);
+    }
+}
